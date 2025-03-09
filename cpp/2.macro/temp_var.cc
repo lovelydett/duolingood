a4bc9f97fa37019e5a6d0a70d5ed57f3323fdbca
lovelydett <xyt@bupt.cn>
2025-03-09 17:41:13 -0400

    [C++] 2.macro using temp var

---
+++ cpp/2.macro/temp_var.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+
+// This can not handle the case when a or b is function call or "a++"
+// Consider using std::max or inline function
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
+
+// If neccessary use temp variables
+#define MAX2(a, b)          \
+    ({                      \
+        decltype(a) _a = a; \
+        decltype(b) _b = b; \
+        _a > _b ? _a : _b;  \
+    })
+
+int main(int argc, char** argv) {
+    int a = 1, b = 1;
+
+    // Correct: 1
+    std::cout << MAX(a, b) << std::endl;
+
+    // Incorrect: should be 2 but got 3
+    std::cout << MAX(++a, b) << std::endl;
+
+    a = 1, b = 1;
+    // This oututs 2, avoid re-executing expression
+    std::cout << MAX2(++a, b) << std::endl;
+
+    return 0;
+}
